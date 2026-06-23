4b3cd4e8c80a2fc658bc3ef8ee17141dac2f61ff
MohammedAH10 <kaninpino1000@gmail.com>
2026-06-23 16:29:33 +0100

    Fix: API client load

--- 01.agentic-rag/code/RAG.py
+++ 01.agentic-rag/code/RAG.py
@@ -10,9 +10,26 @@ load_dotenv()
 documents = load_faq_data()
 index = build_index(documents)
 
-openai_client = OpenAI()
+openai_client = OpenAI(api_key=os.getenv("OPENAI_API_KEY"), base_url="https://openrouter.ai/api/v1")
 
 assistant = RAGBase(index=index, llm_client=openai_client)
 
 answer = assistant.rag("I just discovered this course is it available for me to join now?")
 print(answer)
+
+
+# can override system instruction if we want to
+custom_instructions = """
+You're a course teaching assistant.
+Answer the QUESTION based on the CONTEXT from the FAQ database.
+Use only the facts from the CONTEXT when answering the QUESTION.
+""".strip()
+
+assistant = RAGBase(
+    index=index,
+    llm_client=openai_client,
+    instructions=custom_instructions,
+)
+
+another_answer = assistant.rag("Would i get the certificate if i join the course late?")
+print(another_answer)
